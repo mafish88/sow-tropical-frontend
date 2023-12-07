81384b1ead5553127e89d9c6a5819bf8c187cc04
Michael Fisher <mafish88@icloud.com>
2023-12-07 16:51:23 -0500

    update

--- src/components/PlantsCard.jsx
+++ src/components/PlantsCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  
   Card,
   CardBody,
   CardTitle,
@@ -12,33 +13,25 @@ export default function PlantsCard(props) {
   const { species, genus, pollination } = props;
 
   return (
-    <div className="plant-card">
-      <Card style={{border: "3px soild grey" ,borderRadius: 10 }}>
+    <div className="cards-container">
+      <Card style={{ border: "2px soild green", borderRadius: 2 }}>
         <CardBody>
-          <CardTitle
-            className="text-center"
-            style={{ fontSize: 24, fontWeight: "bold" }}
-          >
-           Species: {species}
+          <CardTitle className="text-center" style={{ fontSize: 15 }}>
+            Species: {species}
           </CardTitle>
-          <CardText
-            className="text-center"
-            style={{ fontSize: 18, fontWeight: "bold" }}
-          >
+          <CardText className="text-center" style={{ fontSize: 15 }}>
             Genus: {genus}
           </CardText>
-          <CardText className="text-center" style={{ fontSize: 16 }}>
+          <CardText className="text-center" style={{ fontSize: 15 }}>
             Propragation: {pollination}
           </CardText>
           <div className="d-flex justify-content-center">
-            <Button variant="primary" style={{ fontSize: 16 }}>
-              
-              
+            <Button variant="primary" style={{ fontSize: 12 }}>
               Fertilizer
             </Button>
           </div>
           <div className="d-flex justify-content-center mt-2">
-            <Button variant="primary" style={{ fontSize: 16 }}>
+            <Button variant="primary" style={{ fontSize: 12 }}>
               Watering
             </Button>
           </div>
--- src/pages/PlantsPage.jsx
+++ src/pages/PlantsPage.jsx
@@ -1,92 +1,83 @@
-import React, { useEffect } from 'react';
-import PlantsCard from '../components/PlantsCard';
-import '../App.css';
-
+import React, { useEffect } from "react";
+import PlantsCard from "../components/PlantsCard";
+import "../App.css";
 
 // fetch data from api
 
-
-
 function PlantsPage() {
-
   const [plants, setPlants] = React.useState([]);
 
   useEffect(() => {
-  fetch ("https://final-project-mf.nn.r.appspot.com/plants")
-  .then((response) => response.json())
-   .then((data) => setPlants(data))
-    .catch((error) => console.log(error));
-  },[])
+    fetch("https://final-project-mf.nn.r.appspot.com/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data))
+      .catch((error) => console.log(error));
+  }, []);
 
- function handleSubmit(event) {
-        event.preventDefault();
-        const name = event.target.species.value;
-        const scientificName = event.target.genus.value;
-        const description = event.target.propergation.value;
-        const plant = { name, scientificName, description };
-        fetch("https://final-project-mf.nn.r.appspot.com/plants", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(plant),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Success:", data);
-                window.location.reload();
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.species.value;
+    const scientificName = event.target.genus.value;
+    const description = event.target.propergation.value;
+    const plant = { name, scientificName, description };
+    fetch("https://final-project-mf.nn.r.appspot.com/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(plant),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
   return (
     <div>
       <h1>Tropical and Exotic Fruit Trees</h1>
       <div
-  className="cards-container"
-  style={{
-    border: "1px solid #ddd",
-    borderBlockStyle: "solid",
-    borderRadius: "5px",
-    padding: "10px",
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-  }}
->
-  {plants?.map((plant) => (
-    <PlantsCard
-      key={plant._id}
-      species={plant.species || plant.name}
-      genus={plant.genus || plant.scientificName}
-      pollination={plant.pollination || plant.propergation || plant.description}
-    />
-  ))}
-</div>
+        className="cards-container"
+        style={{
+          border: "3px solid #ddd",
+          borderBlockStyle: "solid",
+          borderRadius: "5px",
+          padding: "25px",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {plants?.map((plant) => (
+          <PlantsCard
+            key={plant._id}
+            species={plant.species || plant.name}
+            genus={plant.genus || plant.scientificName}
+            pollination={
+              plant.pollination || plant.propergation || plant.description
+            }
+          />
+        ))}
+      </div>
 
       <div>
-         
-            <h1>Add Plant</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="species">Species:</label>
-                <input type="text" id="species" />
-                <label htmlFor="genus">Genus:</label>
-                <input type="text" id="genus" />
-                <label htmlFor="propagation">Propagation:</label>
-                <input type="text" id="propagation" />
-                <button type="submit">Submit</button>
-             
-            </form>
-        </div>
+        <h1>Add Plant</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="species">Species:</label>
+          <input type="text" id="species" />
+          <label htmlFor="genus">Genus:</label>
+          <input type="text" id="genus" />
+          <label htmlFor="propagation">Propagation:</label>
+          <input type="text" id="propagation" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
-
   );
-};
+}
 
 export default PlantsPage;
-
-
-
-
